67ec0393200c094aeecdc3ddde13feca8560f436
QuainK <1324575148@qq.com>
2025-10-18 23:40:36 +0800

    water

--- main.py
+++ main.py
@@ -2,21 +2,21 @@
 clear()
 
 # 耕地
-# for i in range(get_world_size()):
-#     for j in range(get_world_size()):
-#         till()
-#         move(North)
-#     move(East)
+for i in range(get_world_size()):
+    for j in range(get_world_size()):
+        till()
+        move(North)
+    move(East)
 
 # 种植和收获
 while True:
-    if can_harvest():
-        harvest()
-    # for i in range(get_world_size()):
-    #     for j in range(get_world_size()):
-    #         if can_harvest():
-    #             harvest()
-    #         # plant(Entities.Bush)
-    #         # plant(Entities.Carrot)
-    #         move(North)
-    #     move(East)
+    # if can_harvest():
+    #     harvest()
+    for i in range(get_world_size()):
+        for j in range(get_world_size()):
+            if can_harvest():
+                harvest()
+            # plant(Entities.Bush)
+            plant(Entities.Carrot)
+            move(North)
+        move(East)
