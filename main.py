9972ae0d00f6ffde8a4b47c5b7f54fc02657856d
QuainK <1324575148@qq.com>
2025-10-18 23:55:55 +0800

    函数和import

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
-# 清空农场
-clear()
+from __builtins__ import *
 
-# 耕地
-for i in range(get_world_size()):
-    for j in range(get_world_size()):
-        till()
-        move(North)
-    move(East)
 
-# 种植和收获
-while True:
-    # if can_harvest():
-    #     harvest()
-    for i in range(get_world_size()):
-        for j in range(get_world_size()):
-            if can_harvest():
-                harvest()
-            # if (i + j) % 2 == 0:
-            #     plant(Entities.Tree)
-            # plant(Entities.Bush)
-            plant(Entities.Carrot)
-            move(North)
-        move(East)
+def main():
+    # 清空农场
+    clear()
+
+    # 种植和收获
+    while True:
+        for i in range(get_world_size()):
+            for j in range(get_world_size()):
+                if can_harvest():
+                    harvest()
+                if i == 2 or i == 3:
+                    if (i + j) % 2 == 0:
+                        plant(Entities.Tree)
+                elif i == 4 or i == 5:
+                    if get_ground_type() != Grounds.Soil:
+                        till()
+                    plant(Entities.Carrot)
+                move(North)
+            move(East)
+
+
+if __name__ == '__main__':
+    main()
