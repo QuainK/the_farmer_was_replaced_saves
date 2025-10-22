4d361a941b5ce58ef1dce1e713090e2c6ab70b3e
QuainK <1324575148@qq.com>
2025-10-22 23:05:57 +0800

    优化单格、一列、整场（多线程），迷宫

--- maze.py
+++ maze.py
@@ -1,15 +1,30 @@
+import utils
 from __builtins__ import *
 
 
-def main():
-    clear()
+# 单格
+def one():
     while True:
         plant(Entities.Bush)
-        # substance = get_world_size() * 2 ** (num_unlocked(Unlocks.Mazes) - 1)
-        substance = 4
-        use_item(Items.Weird_Substance, substance)
+        # 加成倍率
+        times = 2 ** (num_unlocked(Unlocks.Mazes) - 1)
+        # 就种单格
+        # substance = get_world_size() * times
+        substance = 1 * times
+        if num_items(Items.Weird_Substance) > 0:
+            use_item(Items.Weird_Substance, substance)
+        else:
+            if can_harvest():
+                harvest()
         if get_entity_type() == Entities.Treasure:
             harvest()
+
+
+# 整场
+def main():
+    utils.clear_gently(True)
+    while True:
+        utils.multiple(one)
 
 
 if __name__ == '__main__':
