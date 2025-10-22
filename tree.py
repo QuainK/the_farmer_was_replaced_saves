cbb0faf264ab37c2821dcdc9206564fa2ce935b8
QuainK <1324575148@qq.com>
2025-10-22 22:24:37 +0800

    优化单格、一列、整场（多线程），树木

--- tree.py
+++ tree.py
@@ -2,28 +2,32 @@ import utils
 from __builtins__ import *
 
 
+# 单格
+def one():
+    if can_harvest():
+        harvest()
+    if get_ground_type() != Grounds.Grassland:
+        till()
+    # 棋盘布局种植树木和干草
+    if (get_pos_x() + get_pos_y()) % 2 == 0:
+        plant(Entities.Tree)
+        utils.water()
+
+
 # 一列
-def line():
+def column():
+    utils.move_to(get_pos_x(), 0)
     for _ in range(get_world_size()):
-        if can_harvest():
-            harvest()
-        if (get_pos_x() + get_pos_y()) % 2 == 0:
-            if get_ground_type() != Grounds.Soil:
-                till()
-            plant(Entities.Tree)
-            # utils.water()
-        else:
-            if get_ground_type() != Grounds.Grassland:
-                till()
-        utils.scan_line()
+        one()
+        utils.scan_column()
 
 
+# 整场
 def main():
     utils.move_origin()
     while True:
-        for i in range(get_world_size()):
-            line()
-            move(East)
+        utils.multiple(column)
+        utils.move_origin()
 
 
 if __name__ == '__main__':
